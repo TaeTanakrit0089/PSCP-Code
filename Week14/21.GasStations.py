85acf58d0c8775da8da8763eb5bc93c9d4dfceff
TaeTanakrit0089 <tanakritsupprasit@gmail.com>
2022-11-03 12:55:33 +0700

    Not Finished Yet

--- Week14/21.GasStations.py
+++ Week14/21.GasStations.py
@@ -3,14 +3,33 @@
 
 def lwyrup():
     '''The constitution says you do! And so do I.'''
-    all_distance, full_tank, all_gas = int(input()), int(input()), int(input())
-    gas_station = [int(input()) for i in range(all_gas)]
-    each_length = [gas_station[0]] + [(gas_station[i+1] - gas_station[i]) for i in range(all_gas-1)]
+    all_distance, full_tank, all_gas = float(
+        input()), float(input()), int(input())
+
+    if all_distance <= full_tank:
+        return 0
+
+    gas_station = [float(input()) for _ in range(all_gas)]
+    station_distance = [gas_station[0]] + \
+        [gas_station[i+1] - gas_station[i] for i in range(all_gas-1)] + \
+        [all_distance - gas_station[-1]]
+
     car, count = full_tank, 0
-    for i in range(1, len(each_length)):
-        car -= each_length[i-1]
-        if car < each_length[i+1]:
-            car = full_tank
+    for i in range(len(station_distance)):
+        if i+1 == len(station_distance):
+            break
+
+        car -= station_distance[i]
+        if car < 0:
+            return 'depleted'
+        if car < station_distance[i+1]:
             count += 1
-    print(count)
-lwyrup()
+            car = full_tank
+
+    if car < station_distance[-1]:
+        return 'depleted'
+
+    return count
+
+
+print(lwyrup())
