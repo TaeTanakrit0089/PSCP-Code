b2c2de674a600d5fc4824410ac7682588fd3cb1c
TaeTanakrit0089 <tanakritsupprasit@gmail.com>
2022-11-04 23:09:31 +0700

    Goodman

---
+++ Week15/11.Fever.py
@@ -0,0 +1,15 @@
+'''Saul Goodman'''
+def huell():
+    '''Saul Goodman'''
+    temp = float(input())
+    if temp >= 36 and temp < 38:
+        print('No Fever')
+    elif temp >= 38 and temp < 39:
+        print('Mild Fever')
+    elif temp >= 39 and temp < 40:
+        print('High Fever')
+    else:
+        print('Very High Fever')
+
+
+huell()
